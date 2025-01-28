0bd95177b6ab797ab03eed5052e04a07618b02bc
tom <tom@hippopenny.com>
2025-01-27 16:23:28 -0800

    add another search case

--- scores/update_titles.py
+++ scores/update_titles.py
@@ -3,12 +3,9 @@ import re
 
 def update_markdown_titles(directory):
     for filename in os.listdir(directory):
-        # print(filename)
         if filename.endswith(".md"):
             file_path = os.path.join(directory, filename)
-            print(filename)
             permalink_match = re.match(r'\d{4}-\d{2}-\d{2}-(.+)\.md', filename)
-            print(permalink_match)
             if permalink_match:
                 game_title_kebab = permalink_match.group(1)
                 game_title_words = game_title_kebab.replace('-', ' ').title()
@@ -18,7 +15,9 @@ def update_markdown_titles(directory):
                     content = file.read()
                 
                 updated_content = re.sub(r'title: Game Scores By HippoPenny AI', f'title: {new_title}', content)
+                updated_content = re.sub(r'title: Game Scores By Hippo Penny AI', f'title: {new_title}', content)
 
+                print(updated_content)
                 with open(file_path, 'w') as file:
                     file.write(updated_content)
                 print(f"Updated title in {filename} to: {new_title}")
